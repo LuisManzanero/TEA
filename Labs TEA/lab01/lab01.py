5dca8956a46edfcc308820c6657c8e502ed00f56
LuisManzanero <112212855+LuisManzanero@users.noreply.github.com>
2022-09-12 20:24:13 -0600

    Add files via upload

---
+++ Labs TEA/lab01/lab01.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tecnología agricoka (TEA)
+# Autor: luis.manzanero@est.zamorano.edu
+# Fecha: 2022.09.02
+# Editado: 2022.09.02
+
+print ("Hola, mundo")
---
+++ Labs TEA/lab02/celcius2farenheit.py
@@ -0,0 +1,4 @@
+# TEA
+temperaturaCelsius = float(input("Cual es la temperatura en Celsius"))
+temperaturaFarenheit = (9/5) * temperaturaCelsius +32
+print(temperaturaFarenheit)
